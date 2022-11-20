44971e1c634da4ac60b3903759012159401f8d3d
devingdaniels <devingdaniels@gmail.com>
2022-11-20 05:27:29 -0800

    Set up express and mongoose

---
+++ backend-REST/listify_controller.mjs
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express, { response } from 'express';
+import * as tasks from './listify_model.mjs';
+
+// Express server running on port 3000
+const PORT = process.env.PORT; 
+const app = express();
+
+// Middleware function to parse PUT and POST requests 
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
---
+++ backend-REST/listify_model.mjs
@@ -0,0 +1,30 @@
+// Import dependencies.
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+// Connect based on the .env file parameters.
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+
+// Confirm that the database has connected and print a message in the console.
+db.once("open", (err) => {
+    if(err){
+        res.status(500).json({ error: '500:Connection to the server failed.' });
+    } else  {
+        console.log('Successfully connected to MongoDB!');
+    }
+});
+
+
+// Each schema maps to a MongoDB collection and defines the shape of the documents within that collection.
+const { Schema } = mongoose;
+const projectSchema = new Schema({
+    title: {type: String},     
+    body: [{ body: String, date: Date, comments: String, isComplete: Boolean }]
+});
+
+// To use our schema definition, we need to convert our projectSchema into a Model we can work with.
