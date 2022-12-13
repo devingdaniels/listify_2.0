c3809d2aad78533990a55faa52c90bfd14fe72ca
devingdaniels <devingdaniels@gmail.com>
2022-12-13 14:45:31 -0800

    Refactored jsx and CSS in ProjectForm.js component

--- frontend/src/components/ProjectForm.js
+++ frontend/src/components/ProjectForm.js
@@ -1,4 +1,5 @@
-import { useState }  from 'react'
+// React
+import { useState } from 'react'
 // Redux
 import { useDispatch } from 'react-redux'
 import { createProject }  from '../features/projects/projectSlice'
@@ -8,7 +9,6 @@ function ProjectForm() {
     const dispatch = useDispatch()
     const [title, setTitle] = useState('')
   
-
     const handleSubmit = (e) => { 
       e.preventDefault()
       dispatch(createProject({ title }))      
@@ -16,18 +16,18 @@ function ProjectForm() {
     }
 
     return (      
-        <form onSubmit={handleSubmit}>            
+        <form onSubmit={handleSubmit} className='project-form-container'>
             <input
-                type="text"
-                name='title'
-                value={title}
-                id='title'
-                placeholder='New Project'
-                required
-                onChange={(e) => setTitle(e.target.value)}
-                />
-                <button type='submit'>Add Project</button>
-        </form>    
+              type="text"
+              name='title'
+              value={title}
+              id='title'
+              placeholder='New Project'
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+        <button type='submit'>New Project</button>
+      </form>
   )
 }
 
