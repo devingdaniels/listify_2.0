4391adee8d8520498604d8efcaf53f9c3dfcb5a5
devingdaniels <devingdaniels@gmail.com>
2022-12-13 12:58:42 -0800

    Added input fields to task form

--- frontend/src/components/TaskForm.js
+++ frontend/src/components/TaskForm.js
@@ -5,16 +5,31 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addTaskToProject } from '../features/projects/projectSlice'
 
+// Icons
+import { BsStar } from 'react-icons/bs'
+
 
 function TaskForm({ id }) {
-
-    const dispatch = useDispatch()
-    const [task, setTask] = useState('')
+// State    
+    const [taskData, setTaskData] = useState({
+        title: '',
+        description: '',
+        isFavorite: false
+    })
     const [isEditable, setIsEditable] = useState(false)
-    
-    const toggleEditTask = () => {
-    setIsEditable(isEditable => isEditable = !isEditable)
-  }
+
+    // METHODS
+    const dispatch = useDispatch()
+    const toggleEditTask = () => { setIsEditable(isEditable => isEditable = !isEditable)}
+    const handleOnChange = (e) => {setTaskData((prevState) => ({ ...prevState, [e.target.name]: e.target.value }))}
+    const toggleIsFav = () => {
+        setTaskData({
+            title: taskData.title,
+            description: taskData.description,
+            isFavorite: !taskData.isFavorite
+        })
+        console.log(taskData)
+    }
 
     const onSubmit = (e) => { 
         // Prevent page reload
@@ -22,33 +37,41 @@ function TaskForm({ id }) {
         // Pass ID of project and title of task
         const data = {
             id: id,
-            taskData: task,            
+            taskData: taskData,            
         }
-        // Create payload        
-        dispatch(addTaskToProject(data))
+        // Create payload
+        console.log(taskData)
+        // dispatch(addTaskToProject(data))
         // Reset state
-        setTask('')        
-    }
-    
+        // setTaskData({
+        //     title: '',
+        //     description: '',
+        //     favorite: false
+        // })        
+    }    
+
     if (isEditable) {
         return (    
             <form onSubmit={onSubmit} className='task-form'>
                 <input
                     type="text"
+                    name='title'
+                    id='title'
                     placeholder='Title'
-                    value={task}
-                    onChange={ (e)=>  setTask(e.target.value)}
+                    defaultValue={taskData.title}                    
+                    onChange={handleOnChange}
                 />
                 <textarea
                     type="text"
+                    name='description'
+                    id='description'
                     placeholder='Description'
-                />
-                <button
-                    type="radio"
-                    placeholder='Title'  
-                ></button>                
+                    defaultValue={taskData.description}
+                    onChange={ handleOnChange }
+                />                
+                <BsStar onClick={toggleIsFav} />               
                 <button type='button' className='task-section-button' onClick={() => toggleEditTask()}>Cancel</button>
-                <button type='submit' className='task-section-button' >Add</button>                     
+                <button type='submit' className='task-section-button'>Add</button>                     
             </form>
         )
     }
